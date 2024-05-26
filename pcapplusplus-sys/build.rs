0c15b9145a8094f69e059e0cfdc2d7b5535d2c0f
Izumi Raine <63145238+IzumiRaine@users.noreply.github.com>
2024-05-26 16:26:12 +0200

    Move PcapPlusPlus submodule into the -sys crate

--- pcapplusplus-sys/build.rs
+++ pcapplusplus-sys/build.rs
@@ -1,6 +1,6 @@
 fn main()
 {
-    let pcpp = cmake::build("../PcapPlusPlus");
+    let pcpp = cmake::build("PcapPlusPlus");
     let pcpp_include = pcpp.join("include");
     let pcpp_lib = pcpp.join("lib64");
     println!("cargo:include={}", pcpp_include.display());
